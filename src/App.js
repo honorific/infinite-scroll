a22b88e3aabbabddf7c628e16fa42d810b35ecc0
honorific <honorific.info@gmail.com>
2023-05-06 21:43:26 +0430

    clearig the logs

--- src/App.js
+++ src/App.js
@@ -23,14 +23,12 @@ const App = () => {
       }
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hasMore) {
-          console.log('visible')
           setPageNumber((prev) => prev + 1)
         }
       })
       if (node) {
         observer.current.observe(node)
       }
-      console.log(node)
     },
     [loading, hasMore],
   )
